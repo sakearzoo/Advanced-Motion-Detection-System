2ba6c13bc5149aef787760676ea3682bd92e4d2b
sakearzoo <53253665+sakearzoo@users.noreply.github.com>
2019-03-05 19:11:19 +0530

    Verify PIR Sensor

---
+++ pirtest.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python
+#https://github.com/sakearzoo
+#By Sheikh Nawab Arzoo
+
+import RPi.GPIO as GPIO
+import time
+
+sensorPin = 7
+
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(sensorPin, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+
+prevState = False
+currState = False
+
+while True:
+    time.sleep(0.1)
+    prevState = currState
+    currState = GPIO.input(sensorPin)
+    if currState != prevState:
+        newState = "HIGH" if currState else "LOW"
+        print "GPIO pin %s is %s" % (sensorPin, newState)
