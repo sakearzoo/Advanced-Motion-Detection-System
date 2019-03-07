66af08d43c33ff0c15630a2b0f78e40c3490987d
sakearzoo <53253665+sakearzoo@users.noreply.github.com>
2019-03-07 20:22:13 +0530

    RFID Testing

---
+++ readRFID.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+#https://github.com/sakearzoo
+#By Sheikh Nawab Arzoo
+import sys
+sys.path.insert(0, "/home/pi/pi-rc522/ChipReader")
+import RFID
+import signal
+import time
+
+rdr = RFID.RFID()
+util = rdr.util()
+util.debug = False
+
+while True:
+    #Request tag
+    (error, data) = rdr.request()
+    if not error:
+        print ("\nDetected")
+        
+   (error, uid) = rdr.anticoll()
+        if not error:
+            #Print UID
+            print ("Card read UID: "+str(uid[0])+", "+str(uid[1])+", "+str(uid[2])+", "+str(uid[3]))
+
+            time.sleep(1)
